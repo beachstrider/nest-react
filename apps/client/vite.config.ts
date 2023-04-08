eb5bef76ead7a529a3de6a5891480452050e5837
Jason Mendoza <mernstackdev729@gmail.com>
2023-04-09 00:57:40 +0900

    change: ports

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -5,9 +5,11 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 3000,
+    strictPort: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:3500',
         changeOrigin: true
       }
     }
