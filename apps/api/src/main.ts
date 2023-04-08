52b6f477149b37bf04c5ea93034b6ac4c3531d37
Jason Mendoza <mernstackdev729@gmail.com>
2023-04-09 01:11:23 +0900

    fix: ports

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.setGlobalPrefix('api')
-  await app.listen(3500)
+  await app.listen(80)
 }
 bootstrap()
--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     strictPort: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3500',
+        target: 'http://localhost',
         changeOrigin: true
       }
     }
